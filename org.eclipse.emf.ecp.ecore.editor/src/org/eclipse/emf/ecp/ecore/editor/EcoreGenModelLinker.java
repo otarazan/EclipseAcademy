87591d8fd7b90eda56afc51882bf4c617bde8275
dwsoto <david.soto@web.de>
2013-10-02 13:42:09 +0200

    added the basic interface for the ecore gen model linker

---
+++ org.eclipse.emf.ecp.ecore.editor/src/org/eclipse/emf/ecp/ecore/editor/EcoreGenModelLinker.java
@@ -0,0 +1,11 @@
+package org.eclipse.emf.ecp.ecore.editor;
+
+public class EcoreGenModelLinker {
+
+	public void generateGenModel(String ecorePath, String genModelPath,
+			String modelProjectPath, String editorProjectPath,
+			String editorProjectPath2) {
+
+	}
+
+}
