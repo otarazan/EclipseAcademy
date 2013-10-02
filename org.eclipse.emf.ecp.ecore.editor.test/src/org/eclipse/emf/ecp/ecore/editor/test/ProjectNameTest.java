b2afb9ef2bdb9005e0e8bc8e54a38f8b1190d8b4
Al-Waleed Shihadeh <Wshihadeh@live.com>
2013-10-02 14:34:12 +0200

    FinishProjectHelperClass

--- org.eclipse.emf.ecp.ecore.editor.test/src/org/eclipse/emf/ecp/ecore/editor/test/ProjectNameTest.java
+++ org.eclipse.emf.ecp.ecore.editor.test/src/org/eclipse/emf/ecp/ecore/editor/test/ProjectNameTest.java
@@ -2,52 +2,39 @@ package org.eclipse.emf.ecp.ecore.editor.test;
 
 import static org.junit.Assert.*;
 
-import org.eclipse.core.resources.IFile;
-import org.eclipse.core.resources.IProject;
+
 import org.eclipse.emf.ecp.ecore.editor.ProjectHelper;
-import org.eclipse.ui.IEditorPart;
-import org.eclipse.ui.IFileEditorInput;
-import org.eclipse.ui.PlatformUI;
-import org.eclipse.ui.part.FileEditorInput;
 import org.junit.Test;
 
 public class ProjectNameTest {
 
-	
-//	@Test
-//	public void TestGetProjectName()
-//	{
-		//getting the current project name
-//		IEditorPart  editorPart = PlatformUI.getWorkbench().getActiveWorkbenchWindow().
-//				getActivePage().
-//				getActiveEditor();
-//				String activeProjectName = null;
-//
-//				if(editorPart  != null)
-//				{
-//				IFileEditorInput input = (FileEditorInput)editorPart.getEditorInput();
-//				IFile file = input.getFile();
-//				IProject activeProject = file.getProject();
-//				activeProjectName =activeProject.getName();
-//				}
-//
-//		assertEquals("org.eclipse.ecp.emf.ecore.editor", activeProjectName);
-//	}
+
 	
 	@Test
-	public void TestGetProjectName()
+	public void testGetProjectName()
 	{
-		String string = ProjectHelper.getProjectName("org.eclipse.ecp.emf.ecore.editor");
+		ProjectHelper.setProjectFullName("org.eclipse.ecp.emf.ecore.editor");
+		String string = ProjectHelper.getProjectName();
 		
 		assertEquals("editor", string);
 		
 	}
 	
 	@Test
+	public void testGetProjectNameOnePart()
+	{
+		ProjectHelper.setProjectFullName("org");
+		String string = ProjectHelper.getProjectName();
+		
+		assertEquals("org", string);
+		
+	}
+	
+	@Test
 	public void TestGetNSPrefix()
 	{
-		String string = ProjectHelper.getNSPrefix("org.eclipse.ecp.emf.ecore.editor");
-		
+		ProjectHelper.setProjectFullName("org.eclipse.ecp.emf.ecore.editor");
+		String string = ProjectHelper.getNSPrefix();
 		assertEquals("org.eclipse.ecp.emf.ecore", string);
 		
 	}
@@ -56,7 +43,8 @@ public class ProjectNameTest {
 	@Test
 	public void TestGetNSURL()
 	{
-		String string = ProjectHelper.getNSURL("org.eclipse.ecp.emf.ecore.editor");
+		ProjectHelper.setProjectFullName("org.eclipse.ecp.emf.ecore.editor");
+		String string = ProjectHelper.getNSURL();
 		
 		assertEquals("http://eclipse.org/ecp/emf/ecore/editor", string);
 		
