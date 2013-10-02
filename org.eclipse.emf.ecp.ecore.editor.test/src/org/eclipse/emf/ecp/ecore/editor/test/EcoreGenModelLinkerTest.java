bc0e9c40b07a7ecb13b099168b5fa0b14384185a
dwsoto <david.soto@web.de>
2013-10-02 15:59:14 +0200

    use setup/teardown methods

--- org.eclipse.emf.ecp.ecore.editor.test/src/org/eclipse/emf/ecp/ecore/editor/test/EcoreGenModelLinkerTest.java
+++ org.eclipse.emf.ecp.ecore.editor.test/src/org/eclipse/emf/ecp/ecore/editor/test/EcoreGenModelLinkerTest.java
@@ -4,22 +4,24 @@ import static org.junit.Assert.*;
 
 import java.io.File;
 
+import junit.framework.TestCase;
+
 import org.eclipse.emf.ecp.ecore.editor.EcoreGenModelLinker;
 import org.junit.Test;
 
-public class EcoreGenModelLinkerTest {
+public class EcoreGenModelLinkerTest extends TestCase {
 
 	@Test
 	public void test() throws Exception {
+		String absPath = new File("").getAbsolutePath();
 		EcoreGenModelLinker linker = new EcoreGenModelLinker();
-		String ecorePath = "resource/model/library.ecore";
-		File folder = new File("tmp");
-		folder.mkdir();
-		String genModelPath = "tmp/library.genmodel";
-		String modelProjectPath = "tmp/result";
-		String editProjectPath = "tmp/result.edit";
-		String editorProjectPath = "tmp/result.editor";
-		linker.generateGenModel(ecorePath, genModelPath, modelProjectPath, editProjectPath, editorProjectPath);
+		String ecorePath = absPath + "/resources/model/library.ecore";
+		String genModelPath = absPath + "/tmp/library.genmodel";
+		String modelProjectPath = absPath + "/tmp/result";
+		String editProjectPath = absPath + "/tmp/result.edit";
+		String editorProjectPath = absPath + "/tmp/result.editor";
+		linker.generateGenModel(ecorePath, genModelPath, modelProjectPath,
+				editProjectPath, editorProjectPath);
 		File resultFolder = new File(modelProjectPath);
 		File genModelFile = new File(genModelPath);
 		File projectFile = new File(modelProjectPath + "/.project");
@@ -29,6 +31,17 @@ public class EcoreGenModelLinkerTest {
 		assertTrue(projectFile.exists());
 		assertTrue(sourceFolder.exists());
 	}
-	
-	
+
+	@Override
+	protected void setUp() {
+		File folder = new File("tmp");
+		folder.mkdir();
+		System.out.println(folder.getAbsolutePath());
+	}
+
+	@Override
+	protected void tearDown() {
+		File folder = new File("tmp");
+		folder.delete();
+	}
 }
