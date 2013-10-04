5ed5b66a96409a733e0c9749c2b174869d667f96
dwsoto <david.soto@web.de>
2013-10-04 17:56:01 +0200

    removed second wizard page

--- org.eclipse.emf.ecp.ecore.editor.ui/src/org/eclipse/emf/ecp/ecore/editor/ui/EMFSimpleProjectWizard.java
+++ org.eclipse.emf.ecp.ecore.editor.ui/src/org/eclipse/emf/ecp/ecore/editor/ui/EMFSimpleProjectWizard.java
@@ -18,8 +18,6 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
-import java.util.MissingResourceException;
-import java.util.StringTokenizer;
 
 import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.IProject;
@@ -39,7 +37,6 @@ import org.eclipse.emf.ecore.EClassifier;
 import org.eclipse.emf.ecore.EcoreFactory;
 import org.eclipse.emf.ecore.EcorePackage;
 import org.eclipse.emf.ecore.presentation.EcoreEditorPlugin;
-import org.eclipse.emf.ecore.provider.EcoreEditPlugin;
 import org.eclipse.emf.ecore.resource.URIConverter;
 import org.eclipse.emf.ecp.ecore.editor.ui.operations.CreateModelsWorkspaceModifyOperation;
 import org.eclipse.emf.edit.ui.provider.ExtendedImageRegistry;
@@ -49,16 +46,7 @@ import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.jface.viewers.StructuredSelection;
 import org.eclipse.jface.wizard.IWizardContainer;
 import org.eclipse.jface.wizard.Wizard;
-import org.eclipse.jface.wizard.WizardPage;
-import org.eclipse.swt.SWT;
-import org.eclipse.swt.events.ModifyEvent;
-import org.eclipse.swt.events.ModifyListener;
-import org.eclipse.swt.layout.GridData;
-import org.eclipse.swt.layout.GridLayout;
-import org.eclipse.swt.widgets.Combo;
 import org.eclipse.swt.widgets.Composite;
-import org.eclipse.swt.widgets.Label;
-import org.eclipse.swt.widgets.Text;
 import org.eclipse.ui.IEditorDescriptor;
 import org.eclipse.ui.INewWizard;
 import org.eclipse.ui.IWorkbench;
@@ -83,7 +71,6 @@ public class EMFSimpleProjectWizard extends Wizard implements INewWizard {
 	protected String initialProjectName;
 	protected IStructuredSelection selection;
 	protected WizardNewProjectCreationPage newProjectCreationPage;
-	private EcoreModelWizardInitialObjectCreationPage initialObjectCreationPage;
 	protected List<String> initialObjectNames;
 	protected EcorePackage ecorePackage = EcorePackage.eINSTANCE;
 	protected EcoreFactory ecoreFactory = ecorePackage.getEcoreFactory();
@@ -138,15 +125,6 @@ public class EMFSimpleProjectWizard extends Wizard implements INewWizard {
 				.getString("_UI_EmptyProject_description"));
 
 		addPage(newProjectCreationPage);
-
-		initialObjectCreationPage = new EcoreModelWizardInitialObjectCreationPage(
-				"Whatever2");
-		initialObjectCreationPage.setTitle(EcoreEditorPlugin.INSTANCE
-				.getString("_UI_EcoreModelWizard_label"));
-		initialObjectCreationPage.setDescription(EcoreEditorPlugin.INSTANCE
-				.getString("_UI_Wizard_initial_object_description"));
-
-		addPage(initialObjectCreationPage);
 	}
 
 	@Override
@@ -186,8 +164,6 @@ public class EMFSimpleProjectWizard extends Wizard implements INewWizard {
 				});
 			}
 		}
-		initialObjectCreationPage.setProjectName(newProjectCreationPage
-				.getProjectName());
 		IFile modelFile = getModelFile(newProjectCreationPage.getProjectName(),
 				"test.ecore");
 		try {
@@ -295,261 +271,6 @@ public class EMFSimpleProjectWizard extends Wizard implements INewWizard {
 
 	protected String[] getRequiredBundles() {
 		return new String[] { "org.eclipse.emf.ecore" };
-	}
-
-	/**
-	 * This is the page where the type of object to create is selected. <!--
-	 * begin-user-doc --> <!-- end-user-doc -->
-	 * 
-	 * @generated
-	 */
-	public class EcoreModelWizardInitialObjectCreationPage extends WizardPage {
-		/**
-		 * <!-- begin-user-doc --> <!-- end-user-doc -->
-		 * 
-		 * @generated
-		 */
-		protected Combo initialObjectField;
-
-		/**
-		 * @generated <!-- begin-user-doc --> <!-- end-user-doc -->
-		 */
-		protected List<String> encodings;
-
-		/**
-		 * <!-- begin-user-doc --> <!-- end-user-doc -->
-		 * 
-		 * @generated
-		 */
-		protected Combo encodingField;
-
-		/**
-		 * <!-- begin-user-doc --> <!-- end-user-doc -->
-		 * 
-		 * @generated
-		 */
-		private Text ecoreFileNameText;
-
-		/**
-		 * Pass in the selection. <!-- begin-user-doc --> <!-- end-user-doc -->
-		 * 
-		 * @generated
-		 */
-		public EcoreModelWizardInitialObjectCreationPage(String pageId) {
-			super(pageId);
-		}
-
-		/**
-		 * <!-- begin-user-doc --> <!-- end-user-doc -->
-		 * 
-		 * @generated
-		 */
-		public void createControlGen(Composite parent) {
-			Composite composite = new Composite(parent, SWT.NONE);
-			{
-				GridLayout layout = new GridLayout();
-				layout.numColumns = 1;
-				layout.verticalSpacing = 12;
-				composite.setLayout(layout);
-
-				GridData data = new GridData();
-				data.verticalAlignment = GridData.FILL;
-				data.grabExcessVerticalSpace = true;
-				data.horizontalAlignment = GridData.FILL;
-				composite.setLayoutData(data);
-			}
-
-			Label fileNameLabel = new Label(composite, SWT.LEFT);
-			{
-				fileNameLabel.setText("File Name");
-
-				GridData data = new GridData();
-				data.horizontalAlignment = GridData.FILL;
-				fileNameLabel.setLayoutData(data);
-			}
-			ecoreFileNameText = new Text(composite, SWT.BORDER);
-			{
-				GridData data = new GridData();
-				data.horizontalAlignment = GridData.FILL;
-				data.grabExcessHorizontalSpace = true;
-				ecoreFileNameText.setLayoutData(data);
-			}
-
-			Label containerLabel = new Label(composite, SWT.LEFT);
-			{
-				containerLabel.setText(EcoreEditorPlugin.INSTANCE
-						.getString("_UI_ModelObject"));
-
-				GridData data = new GridData();
-				data.horizontalAlignment = GridData.FILL;
-				containerLabel.setLayoutData(data);
-			}
-
-			initialObjectField = new Combo(composite, SWT.BORDER);
-			{
-				GridData data = new GridData();
-				data.horizontalAlignment = GridData.FILL;
-				data.grabExcessHorizontalSpace = true;
-				initialObjectField.setLayoutData(data);
-			}
-
-			for (String objectName : getInitialObjectNames()) {
-				initialObjectField.add(getLabel(objectName));
-			}
-
-			if (initialObjectField.getItemCount() == 1) {
-				initialObjectField.select(0);
-			}
-			initialObjectField.addModifyListener(validator);
-
-			Label encodingLabel = new Label(composite, SWT.LEFT);
-			{
-				encodingLabel.setText(EcoreEditorPlugin.INSTANCE
-						.getString("_UI_XMLEncoding"));
-
-				GridData data = new GridData();
-				data.horizontalAlignment = GridData.FILL;
-				encodingLabel.setLayoutData(data);
-			}
-			encodingField = new Combo(composite, SWT.BORDER);
-			{
-				GridData data = new GridData();
-				data.horizontalAlignment = GridData.FILL;
-				data.grabExcessHorizontalSpace = true;
-				encodingField.setLayoutData(data);
-			}
-
-			for (String encoding : getEncodings()) {
-				encodingField.add(encoding);
-			}
-
-			encodingField.select(0);
-			encodingField.addModifyListener(validator);
-
-			setPageComplete(validatePage());
-			setControl(composite);
-		}
-
-		public void createControl(Composite parent) {
-			createControlGen(parent);
-			initialObjectField.setText("EPackage");
-			ecoreFileNameText.setText("model.ecore");
-		}
-
-		/**
-		 * <!-- begin-user-doc --> <!-- end-user-doc -->
-		 * 
-		 * @generated
-		 */
-		protected ModifyListener validator = new ModifyListener() {
-			public void modifyText(ModifyEvent e) {
-				setPageComplete(validatePage());
-			}
-		};
-
-		private String projectName;
-
-		/**
-		 * <!-- begin-user-doc --> <!-- end-user-doc -->
-		 * 
-		 * @generated
-		 */
-		protected boolean validatePage() {
-			return getInitialObjectName() != null
-					&& getEncodings().contains(encodingField.getText());
-		}
-
-		/**
-		 * <!-- begin-user-doc --> <!-- end-user-doc -->
-		 * 
-		 * @generated
-		 */
-		@Override
-		public void setVisible(boolean visible) {
-			super.setVisible(visible);
-			if (visible) {
-				if (initialObjectField.getItemCount() == 1) {
-					initialObjectField.clearSelection();
-					encodingField.setFocus();
-				} else {
-					encodingField.clearSelection();
-					initialObjectField.setFocus();
-				}
-			}
-		}
-
-		/**
-		 * <!-- begin-user-doc --> <!-- end-user-doc -->
-		 * 
-		 * @generated
-		 */
-		public String getInitialObjectName() {
-			String label = initialObjectField.getText();
-
-			for (String name : getInitialObjectNames()) {
-				if (getLabel(name).equals(label)) {
-					return name;
-				}
-			}
-			return null;
-		}
-
-		/**
-		 * <!-- begin-user-doc --> <!-- end-user-doc -->
-		 * 
-		 * @generated
-		 */
-		public String getEncoding() {
-			return encodingField.getText();
-		}
-
-		/**
-		 * Returns the label for the specified type name. <!-- begin-user-doc
-		 * --> <!-- end-user-doc -->
-		 * 
-		 * @generated
-		 */
-		protected String getLabel(String typeName) {
-			try {
-				return EcoreEditPlugin.INSTANCE.getString("_UI_" + typeName
-						+ "_type");
-			} catch (MissingResourceException mre) {
-				EcoreEditorPlugin.INSTANCE.log(mre);
-			}
-			return typeName;
-		}
-
-		/**
-		 * <!-- begin-user-doc --> <!-- end-user-doc -->
-		 * 
-		 * @generated
-		 */
-		protected Collection<String> getEncodings() {
-			if (encodings == null) {
-				encodings = new ArrayList<String>();
-				for (StringTokenizer stringTokenizer = new StringTokenizer(
-						EcoreEditorPlugin.INSTANCE
-								.getString("_UI_XMLEncodingChoices")); stringTokenizer
-						.hasMoreTokens();) {
-					encodings.add(stringTokenizer.nextToken());
-				}
-			}
-			return encodings;
-		}
-
-		public String getEcoreFileNameText() {
-			return ecoreFileNameText.getText();
-		}
-
-		public void setProjectName(String projectName) {
-			this.projectName = projectName;
-
-		}
-
-		public String getProjectName() {
-			return projectName;
-		}
-
 	}
 
 	protected Collection<String> getInitialObjectNames() {
