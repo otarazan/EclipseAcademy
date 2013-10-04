78ca19c410dcc036936c8fd7c2e03d81192c47fb
Güven <guveni@gmail.com>
2013-10-04 16:02:07 +0200

    after samewokspace

--- org.eclipse.emf.ecp.view.treemasterview.ui.test/src/org/eclpise/emf/ecp/view/treemasterview/ui/test/AbstractTreeMasterViewTest.java
+++ org.eclipse.emf.ecp.view.treemasterview.ui.test/src/org/eclpise/emf/ecp/view/treemasterview/ui/test/AbstractTreeMasterViewTest.java
@@ -3,6 +3,7 @@ package org.eclpise.emf.ecp.view.treemasterview.ui.test;
 import static org.junit.Assert.*;
 
 import org.eclipse.emf.ecore.EClass;
+import org.eclipse.emf.ecp.ui.view.test.HierarchyViewModelHandle;
 import org.junit.Test;
 
 public class AbstractTreeMasterViewTest {
@@ -11,10 +12,15 @@ public class AbstractTreeMasterViewTest {
 	
 	@Test
 	public void testTreeMasterViewWithoutChildren() {
-//		final HierarchyViewModelHandle horizontalHandle = createHorizontalWithoutChildren();
-//		final Node<Renderable> node = buildNode(horizontalHandle.getRoot());
-//		assertEquals(1, ViewTestHelper.countNodes(node));
-//		assertEquals(horizontalHandle.getRoot(), node.getRenderable());
+		final HierarchyViewModelHandle horizontalHandle = createHorizontalWithoutChildren();
+		final Node<Renderable> node = buildNode(horizontalHandle.getRoot());
+		assertEquals(1, ViewTestHelper.countNodes(node));
+		assertEquals(horizontalHandle.getRoot(), node.getRenderable());
+	}
+
+	private HierarchyViewModelHandle createHorizontalWithoutChildren() {
+		// TODO Auto-generated method stub
+		return null;
 	}
 
 	
