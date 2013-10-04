d78b3859023d340cb7e4e977569a97b576ca2573
Güven <guveni@gmail.com>
2013-10-04 15:49:17 +0200

    changed all

---
+++ org.eclipse.emf.ecp.view.treemasterview.model.tests/src/org/eclipse/emf/ecp/view/treemasterview/model/treemasterview/tests/TreemasterviewAllTests.java
@@ -0,0 +1,46 @@
+/**
+ */
+package org.eclipse.emf.ecp.view.treemasterview.model.treemasterview.tests;
+
+import junit.framework.Test;
+import junit.framework.TestSuite;
+
+import junit.textui.TestRunner;
+
+/**
+ * <!-- begin-user-doc -->
+ * A test suite for the '<em><b>Treemasterview</b></em>' model.
+ * <!-- end-user-doc -->
+ * @generated
+ */
+public class TreemasterviewAllTests extends TestSuite {
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public static void main(String[] args) {
+		TestRunner.run(suite());
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public static Test suite() {
+		TestSuite suite = new TreemasterviewAllTests("Treemasterview Tests");
+		return suite;
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public TreemasterviewAllTests(String name) {
+		super(name);
+	}
+
+} //TreemasterviewAllTests
--- org.eclipse.emf.ecp.view.treemasterview.ui.test/src/org/eclpise/emf/ecp/view/treemasterview/ui/test/AbstractTreeMasterViewTest.java
+++ org.eclipse.emf.ecp.view.treemasterview.ui.test/src/org/eclpise/emf/ecp/view/treemasterview/ui/test/AbstractTreeMasterViewTest.java
@@ -2,13 +2,21 @@ package org.eclpise.emf.ecp.view.treemasterview.ui.test;
 
 import static org.junit.Assert.*;
 
+import org.eclipse.emf.ecore.EClass;
 import org.junit.Test;
 
 public class AbstractTreeMasterViewTest {
 
+	private static EClass horizontalClass;
+	
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testTreeMasterViewWithoutChildren() {
+//		final HierarchyViewModelHandle horizontalHandle = createHorizontalWithoutChildren();
+//		final Node<Renderable> node = buildNode(horizontalHandle.getRoot());
+//		assertEquals(1, ViewTestHelper.countNodes(node));
+//		assertEquals(horizontalHandle.getRoot(), node.getRenderable());
 	}
 
+	
+	
 }
