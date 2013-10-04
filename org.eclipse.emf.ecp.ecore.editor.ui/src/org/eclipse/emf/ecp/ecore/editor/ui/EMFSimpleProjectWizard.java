aea99e96ec565c1ebb60c78f356fcffd46a58beb
dwsoto <david.soto@web.de>
2013-10-04 17:51:38 +0200

    commit ....

--- org.eclipse.emf.ecp.ecore.editor.ui/src/org/eclipse/emf/ecp/ecore/editor/ui/EMFSimpleProjectWizard.java
+++ org.eclipse.emf.ecp.ecore.editor.ui/src/org/eclipse/emf/ecp/ecore/editor/ui/EMFSimpleProjectWizard.java
@@ -17,9 +17,7 @@ import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import java.util.MissingResourceException;
 import java.util.StringTokenizer;
 
@@ -35,26 +33,15 @@ import org.eclipse.emf.codegen.ecore.Generator;
 import org.eclipse.emf.codegen.ecore.genmodel.provider.GenModelEditPlugin;
 import org.eclipse.emf.codegen.util.CodeGenUtil;
 import org.eclipse.emf.common.CommonPlugin;
-import org.eclipse.emf.common.EMFPlugin.EclipsePlugin;
 import org.eclipse.emf.common.util.URI;
 import org.eclipse.emf.ecore.EClass;
 import org.eclipse.emf.ecore.EClassifier;
-import org.eclipse.emf.ecore.ENamedElement;
-import org.eclipse.emf.ecore.EObject;
-import org.eclipse.emf.ecore.EPackage;
 import org.eclipse.emf.ecore.EcoreFactory;
 import org.eclipse.emf.ecore.EcorePackage;
-import org.eclipse.emf.ecore.plugin.EcorePlugin;
 import org.eclipse.emf.ecore.presentation.EcoreEditorPlugin;
 import org.eclipse.emf.ecore.provider.EcoreEditPlugin;
-import org.eclipse.emf.ecore.resource.Resource;
-import org.eclipse.emf.ecore.resource.ResourceSet;
 import org.eclipse.emf.ecore.resource.URIConverter;
-import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
-import org.eclipse.emf.ecore.xmi.XMLResource;
-import org.eclipse.emf.ecp.ecore.editor.IEcoreGenModelLinker;
-import org.eclipse.emf.ecp.ecore.editor.factory.EcoreGenModelLinkerFactory;
-import org.eclipse.emf.ecp.ecore.editor.util.ProjectHelper;
+import org.eclipse.emf.ecp.ecore.editor.ui.operations.CreateModelsWorkspaceModifyOperation;
 import org.eclipse.emf.edit.ui.provider.ExtendedImageRegistry;
 import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.jface.viewers.ISelection;
@@ -100,7 +87,6 @@ public class EMFSimpleProjectWizard extends Wizard implements INewWizard {
 	protected List<String> initialObjectNames;
 	protected EcorePackage ecorePackage = EcorePackage.eINSTANCE;
 	protected EcoreFactory ecoreFactory = ecorePackage.getEcoreFactory();
-	private ProjectHelper projectHelper = new ProjectHelper();
 
 	@Override
 	public void setContainer(IWizardContainer wizardContainer) {
@@ -145,8 +131,6 @@ public class EMFSimpleProjectWizard extends Wizard implements INewWizard {
 								"org.eclipse.ui.resourceWorkingSetPage" });
 			}
 		};
-
-		projectHelper.setProjectFullName(initialProjectName);
 		newProjectCreationPage.setInitialProjectName(initialProjectName);
 		newProjectCreationPage.setTitle(GenModelEditPlugin.INSTANCE
 				.getString("_UI_EmptyProject_title"));
@@ -204,102 +188,18 @@ public class EMFSimpleProjectWizard extends Wizard implements INewWizard {
 		}
 		initialObjectCreationPage.setProjectName(newProjectCreationPage
 				.getProjectName());
-		performFinish2();
-
-		return true;
-	}
-
-	protected EObject createInitialModel() {
-		EClass eClass = (EClass) ecorePackage
-				.getEClassifier(initialObjectCreationPage
-						.getInitialObjectName());
-		EObject rootObject = ecoreFactory.create(eClass);
-
-		// We can't have a null name, in case we're using EMOF serialization.
-		if (rootObject instanceof ENamedElement) {
-			((ENamedElement) rootObject).setName("");
-		}
-		return rootObject;
-	}
-
-	protected String modelString;
-
-	public boolean performFinish2() {
-		projectHelper.setProjectFullName(newProjectCreationPage
-				.getProjectName());
+		IFile modelFile = getModelFile(newProjectCreationPage.getProjectName(),
+				"test.ecore");
 		try {
-			// Remember the file.
-			final IFile modelFile = getModelFile();
-			final ProjectHelper iProjectHelper = projectHelper;
 
 			// Do the work within an operation.
-			//
-			WorkspaceModifyOperation operation = new WorkspaceModifyOperation() {
-				@Override
-				protected void execute(IProgressMonitor progressMonitor) {
-					try {
-						// Create a resource set
-						//
-						ResourceSet resourceSet = new ResourceSetImpl();
-						resourceSet
-								.getURIConverter()
-								.getURIMap()
-								.putAll(EcorePlugin.computePlatformURIMap(true));
+			WorkspaceModifyOperation createModelsOperation = new CreateModelsWorkspaceModifyOperation(
+					modelFile, "EPackage", "UTF-8",
+					newProjectCreationPage.getProjectName());
 
-						// Get the URI of the model file.
-						//
-						URI fileURI = URI.createPlatformResourceURI(modelFile
-								.getFullPath().toString(), true);
-
-						// Create a resource for this file. Don't specify a
-						// content type, as it could be Ecore or EMOF.
-						//
-						Resource resource = resourceSet.createResource(fileURI);
-
-						// Add the initial model object to the contents.
-						//
-						EObject rootObject = createInitialModel();
-						if (rootObject != null) {
-							resource.getContents().add(rootObject);
-						}
-
-						// Save the contents of the resource to the file system.
-						Map<Object, Object> options = new HashMap<Object, Object>();
-						options.put(XMLResource.OPTION_ENCODING,
-								initialObjectCreationPage.getEncoding());
-						options.put(Resource.OPTION_LINE_DELIMITER,
-								Resource.OPTION_LINE_DELIMITER_UNSPECIFIED);
-						EPackage ecorepack = EcorePackage.eINSTANCE;
-						ecorepack = (EPackage) resource.getContents().get(0);
-						ecorepack.setName(iProjectHelper.getProjectName());
-						ecorepack.setNsPrefix(iProjectHelper.getNSPrefix());
-						ecorepack.setNsURI(iProjectHelper.getNSURL());
-						resource.save(options);
-						IEcoreGenModelLinker linker = EcoreGenModelLinkerFactory
-								.getEcoreGenModelLinker();
-						String genModelPath = ResourcesPlugin.getWorkspace()
-								.getRoot().getFile(modelFile.getLocation())
-								.getFullPath().removeLastSegments(1).toFile().getAbsolutePath() + "/model.genmodel";
-						String ecorePath = ResourcesPlugin.getWorkspace()
-								.getRoot().getFile(modelFile.getLocation())
-								.getFullPath().toFile().getAbsolutePath();
-						String projectPath = ResourcesPlugin.getWorkspace()
-								.getRoot().getFile(modelFile.getLocation())
-								.getFullPath().removeLastSegments(2).toFile().getAbsolutePath();
-						linker.generateGenModel(ecorePath, genModelPath,
-								projectPath, projectPath, projectPath);
-					} catch (Exception exception) {
-						EcoreEditorPlugin.INSTANCE.log(exception);
-					} finally {
-						progressMonitor.done();
-					}
-				}
-			};
-
-			getContainer().run(false, false, operation);
+			getContainer().run(false, false, createModelsOperation);
 
 			// Select the new file resource in the current view.
-			//
 			IWorkbenchWindow workbenchWindow = workbench
 					.getActiveWorkbenchWindow();
 			IWorkbenchPage page = workbenchWindow.getActivePage();
@@ -337,6 +237,8 @@ public class EMFSimpleProjectWizard extends Wizard implements INewWizard {
 			return false;
 		}
 	}
+
+	protected String modelString;
 
 	public void modifyWorkspace(IProgressMonitor progressMonitor)
 			throws CoreException, UnsupportedEncodingException, IOException {
@@ -531,8 +433,7 @@ public class EMFSimpleProjectWizard extends Wizard implements INewWizard {
 		public void createControl(Composite parent) {
 			createControlGen(parent);
 			initialObjectField.setText("EPackage");
-			ecoreFileNameText.setText(projectHelper.getProjectName()
-					+ "model.ecore");
+			ecoreFileNameText.setText("model.ecore");
 		}
 
 		/**
@@ -668,10 +569,12 @@ public class EMFSimpleProjectWizard extends Wizard implements INewWizard {
 		return initialObjectNames;
 	}
 
-	private IFile getModelFile() {
-		String projectName = newProjectCreationPage.getProjectName()
-				+ "/model/" + initialObjectCreationPage.getEcoreFileNameText();
-		Path path = new Path(new File(projectName).getPath());
-		return ResourcesPlugin.getWorkspace().getRoot().getFile(path);
+	private IFile getModelFile(String projectName, String fileName) {
+		return ResourcesPlugin
+				.getWorkspace()
+				.getRoot()
+				.getFile(
+						new Path(new File(projectName + "/model/" + fileName)
+								.getPath()));
 	}
 }
